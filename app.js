cf1df673046ac72456f1b1e2ed25ca8611dac530
Induni11 <indunisankalpani6@gmail.com>
2026-02-16 12:34:25 +0530

    Added HTTP GET request example

--- app.js
+++ app.js
@@ -21,3 +21,27 @@ http.createServer(function (req, res) {
     res.write('Hello World from the server!');
     res.end();
 }).listen(8080, () => console.log('Server running on http://localhost:8080'));
+
+
+//HTTP Request
+
+const https = require('https');
+
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+  let data = '';
+
+  // A chunk of data has been received
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on('error', (err) => {
+  console.log("Error: " + err.message);
+});
+
+
